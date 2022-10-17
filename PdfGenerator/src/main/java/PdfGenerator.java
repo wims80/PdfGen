9a341b33c8807e2952d26c450bb21ee6654988a2
wims80 <wims80@gmail.com>
2022-10-17 21:30:16 +0200

    update

--- PdfGenerator/src/main/java/PdfGenerator.java
+++ PdfGenerator/src/main/java/PdfGenerator.java
@@ -1,20 +1,39 @@
-import java.io.InputStream;
-import java.io.FileInputStream;
+import java.io.*;
+
 import org.odftoolkit.odfdom.doc.*;
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
 
 public class PdfGenerator {
     public static void main(String[] args) {
         PdfGenerator pg = new PdfGenerator();
+        pg.generateODT();
         pg.generatePdf();
     }
 
     public void generatePdf() {
         try {
-            InputStream in = new FileInputStream("bilforsikring.odt");
-            OdfTextDocument odt = OdfTextDocument.loadDocument(in);
-            //OdfTextDocument odt = OdfTextDocument.newTextDocument();
+            InputStream in = PdfGenerator.class.getClassLoader().getResourceAsStream("Generated_odf.odt");
+
+            IXDocReport report = XDocReportRegistry.getRegistry().loadReport(
+                    in, TemplateEngineKind.Freemarker);
         } catch (Exception e) {
             System.out.println("Error! " + e.toString());
+        }
+    }
+
+    public void generateODT() {
+        try {
+            OdfTextDocument otd = OdfTextDocument.newTextDocument();
+            otd.addText("First line\n");
+            otd.addText("Second line\n");
+            otd.save(new File("Generated_odf1.odt"));
+            otd.close();
+        } catch (Exception e) {
+            System.out.println("Error generating ODT: " + e.toString());
         }
     }
 }
