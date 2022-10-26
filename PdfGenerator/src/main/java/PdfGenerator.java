3bdf64c116536fabfc0f14a38be36a58beda5db1
wims80 <wims80@gmail.com>
2022-10-26 16:19:58 +0200

    Husforsikring template complete

--- PdfGenerator/src/main/java/PdfGenerator.java
+++ PdfGenerator/src/main/java/PdfGenerator.java
@@ -30,10 +30,10 @@ import fr.opensagres.xdocreport.template.velocity.internal.VelocityTemplateEngin
 public class PdfGenerator {
     public static void main(String[] args) {
         PdfGenerator pg = new PdfGenerator();
+        pg.genHusdataAndPDF();
         //pg.genBildataAndPDF();
-        //pg.genHusdataAndPDF();
         //pg.genReisedataAndPDF();
-        pg.genInnbodataAndPDF();
+        //pg.genInnbodataAndPDF();
     }
 
     public void genInnbodataAndPDF() {
@@ -157,7 +157,7 @@ public class PdfGenerator {
         List<String> innbruddsAlarmEgenskaper = new ArrayList<String>();
         innbruddsAlarmEgenskaper.add("fg_godkjent");
         innbruddsAlarmEgenskaper.add("varsler_vaktselskap");
-        hi.setInnbruddsalarmEgenskap(innbruddsAlarmEgenskaper);
+        //hi.setInnbruddsalarmEgenskap(innbruddsAlarmEgenskaper);
         hi.setBrannalarm("true");
         List<String> brannAlarmEgenskaper = new ArrayList<String>();
         brannAlarmEgenskaper.add("fg_godkjent");
@@ -246,186 +246,9 @@ public class PdfGenerator {
         generatePDF(bi, tilbud, "bil");
 
     }
-/*
-    public void generatePdf(String fileName) {
-        System.out.println("DEBUG: IN generateSpecPdf2(): START");
-        try {
-
-            InputStream in = new FileInputStream(fileName);
-            System.out.println("DEBUG: In generateSpecPdf2(): AFTER InputStream");
-            IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, "ReportId", TemplateEngineKind.Freemarker);
-            FieldsMetadata metadata = report.createFieldsMetadata();
-            metadata.load("tilbud", Tilbud.class, true);
-
-            System.out.println("DEBUG: Setting context");
-            IContext context = report.createContext();
-            context.put("calculatorUseDate", java.time.LocalDate.now().format(DateTimeFormatter
-                    .ofLocalizedDate(FormatStyle.LONG)));
-
-            BilInput bi = new BilInput();
-            bi.setMellomnavn("Wim");
-            bi.setEtternavn("Sjøholm");
-            bi.setFoedselsnr("27/05/80");
-            bi.setPostnr("0482");
-
-            bi.setRegistreringsnummer("EC58238");
-            bi.setTyverialarm("false");
-            bi.setGjenfinning("false");
-
-            bi.setLeasing("false");
-            bi.setForsteBil("false");
-            bi.setBonus("70:fire_aar");
-            bi.setDekning("kasko");
-            bi.setKjoerelengde("12000");
-            bi.setAlderYngsteFoerer("21");
-            bi.setPantIBil("false");
-            bi.setAntallBilskader("0");
-            bi.setParkeringsforhold("egen_laast_garasje");
-            bi.setDagensSelskap("ingen");
-
-            context.putMap(convertObjectToMap(bi));
-
-            List<Tilbud> tilbud = new ArrayList<Tilbud>();
-            tilbud.add(new Tilbud("Utrygg", "Bilforsikring", "Info 1", "5000", "12000", "1337"));
-            tilbud.add(new Tilbud("Else", "Bil", "Info 2", "5000", "12000", "420"));
-            tilbud.add(new Tilbud("SlukketBrann", "Bil", "Info 3", "5000", "12000", "6969"));
-            //context.putMap(convertObjectToMap(tilbud));
-            context.put("tilbud", tilbud);
-
-            System.out.println("DEBUG: Setting options");
-            Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.ODFDOM);
-
-            System.out.println("DEBUG: Generating PDF");
-            FileOutputStream fos = new FileOutputStream("document.pdf");
-            report.convert(context, options, fos);
-            in.close();
-            fos.close();
-
-            System.out.println("DEBUG: PDF generated!");
-        } catch (Exception e) {
-            System.out.println("Error! " + e.toString());
-        }
-    }
-
-    public void convertFTLtoPDF() {
-        try {
-            InputStream in = new FileInputStream(new File("bilforsikring.ftl"));
-            IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Freemarker);
-
-            IContext context = report.createContext();
-
-            context.put("calculatorUseDate", java.time.LocalDate.now().format(DateTimeFormatter
-                    .ofLocalizedDate(FormatStyle.LONG)));
-
-            BilInput bi = new BilInput();
-            bi.setMellomnavn("Wim");
-            bi.setEtternavn("Sjøholm");
-            bi.setFoedselsnr("27/05/80");
-            bi.setPostnr("0482");
-
-            bi.setRegistreringsnummer("EC58238");
-            bi.setTyverialarm("false");
-            bi.setGjenfinning("false");
-
-            bi.setLeasing("false");
-            bi.setForsteBil("false");
-            bi.setBonus("70:fire_aar");
-            bi.setDekning("kasko");
-            bi.setKjoerelengde("12000");
-            bi.setAlderYngsteFoerer("21");
-            bi.setPantIBil("false");
-            bi.setAntallBilskader("0");
-            bi.setParkeringsforhold("egen_laast_garasje");
-            bi.setDagensSelskap("ingen");
-
-            context.putMap(convertObjectToMap(bi));
-
-            List<Tilbud> tilbud = new ArrayList<Tilbud>();
-            tilbud.add(new Tilbud("Utrygg", "Bilforsikring", "Info 1", "5000", "12000", "1337"));
-            tilbud.add(new Tilbud("Else", "Bil", "Info 2", "5000", "12000", "420"));
-            tilbud.add(new Tilbud("SlukketBrann", "Bil", "Info 3", "5000", "12000", "6969"));
-            //context.putMap(convertObjectToMap(tilbud));
-            context.put("tilbud", tilbud);
-
-            // 3) Set PDF as format converter
-            Options options = Options.getTo(ConverterTypeTo.XHTML);
-
-            // 3) Generate report by merging Java model with the ODT and convert it to PDF
-            OutputStream out = new FileOutputStream(new File("bilforsikring.xhtml"));
-            report.convert(context, options, out);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void convertBil() {
-        try {
-            InputStream in = new FileInputStream(new File("bilforsikring.odt"));
-            IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
-
-            FieldsMetadata metadata = report.createFieldsMetadata();
-            metadata.load( "tilbud", Tilbud.class, true );
 
 
-            IContext context = report.createContext();
 
-            context.put("calculatorUseDate", java.time.LocalDate.now().format(DateTimeFormatter
-                    .ofLocalizedDate(FormatStyle.LONG)));
-
-            BilInput bi = new BilInput();
-            bi.setMellomnavn("Wim");
-            bi.setEtternavn("Sjøholm");
-            bi.setFoedselsnr("27/05/80");
-            bi.setPostnr("0482");
-
-            bi.setRegistreringsnummer("EC58238");
-            bi.setTyverialarm("false");
-            bi.setGjenfinning("false");
-
-            bi.setBilmerkeNavn("Volkswagen");
-            bi.setRegistreringsaar("1995");
-            bi.setModellNavn("ID.4");
-            bi.setModellVariantNavn("77kWh 204hk Business");
-            bi.setModellaar("2007");
-
-            bi.setLeasing("false");
-            bi.setForsteBil("true");
-            bi.setBonus("70:fire_aar");
-            bi.setDekning("kasko");
-            bi.setKjoerelengde("12000");
-            bi.setAlderYngsteFoerer("21");
-            bi.setPantIBil("false");
-            bi.setAntallBilskader("0");
-            bi.setParkeringsforhold("egen_laast_garasje");
-            bi.setDagensSelskap("ingen");
-
-            List<String> foreninger = new ArrayList<String>();
-            foreninger.add("KOL");
-            foreninger.add("Norsk Journalistlag");
-            foreninger.add("Ansatt kommune");
-            bi.setForeningsMedlemsskap(foreninger);
-
-            context.putMap(convertObjectToMap(bi));
-
-            List<Tilbud> tilbud = new ArrayList<Tilbud>();
-            tilbud.add(new Tilbud("Utrygg", "Bilforsikring", "Info 1", "5000", "12000", "1337"));
-            tilbud.add(new Tilbud("Else", "Bil", "Info 2", "5000", "12000", "420"));
-            tilbud.add(new Tilbud("SlukketBrann", "Bil", "Info 3", "5000", "12000", "6969"));
-            //context.putMap(convertObjectToMap(tilbud));
-            context.put("tilbud", tilbud);
-
-            // 3) Set PDF as format converter
-            Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.ODFDOM);
-
-            // 3) Generate report by merging Java model with the ODT and convert it to PDF
-            OutputStream out = new FileOutputStream(new File("bilforsikring.pdf"));
-            report.convert(context, options, out);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
- */
     public void generatePDF(CalculatorInput calcInput, List<Tilbud> tilbud, String lob) {
         String filnavn = lob + "forsikring";
         try {
